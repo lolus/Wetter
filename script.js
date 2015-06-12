693292c14c2dc1403f80812e8e3551c888cb10f0
Luca Signoroni <lsgrafik@gmail.com>
2015-06-12 09:18:22 +0200

    update script

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ $(document).ready(function(){
 	navigator.geolocation.getCurrentPosition(function(position)){
 		var koordinaten = {longitude: position.coords.longitude, latitude: position.coords.latitude};
 	
-});
+
 
 $ajax({
-	url: 'https://api.forecast.io/forecast/fab736851cb5b4f591fac4a86ad0169c/' +koordinaten.latitude+','+koordinaten.longitude
+	url: 'https://api.forecast.io/forecast/fab736851cb5b4f591fac4a86ad0169c/' +koordinaten.latitude+','+koordinaten.longitude,
 	data {
 		units: 'si',
 		lang: 'de'
 	},
 	dataType: 'jsonp'
 
-}).done(function() {
+}).done(function(data) {
 
 	console.log(data);
-}
+		)};
+
+	});
+
+});
